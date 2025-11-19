2fb9404a2bb1c00e953ec99d66dd30a4363c0e13
Lasermorph <stefanashansen@hotmail.com>
2025-11-19 16:42:32 +0100

    added calendar

---
+++ Pages/Calendar/CalendarView.cshtml.cs
@@ -0,0 +1,144 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class CalendarViewModel : PageModel
+    {
+        public DateTime CurrentTime { get; set; } = DateTime.Now;
+        public int DaysInMonth { get; set; } = 0;
+        public int Year { get; set; } = 0;
+        public int Month { get; set; } = 0;
+        public string MonthStr { get; set; } = "";
+        public int FirstDayOfMonth { get; set; } = 0;
+        public List<string> WeekNames { get; set; }
+
+        public CalendarViewModel()
+        {
+            DaysInMonth = DateTime.DaysInMonth(CurrentTime.Year, CurrentTime.Month);
+            Year = CurrentTime.Year;
+            Month = CurrentTime.Month;
+            WeekNames = new List<string>{"Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"};
+        }
+        public void OnGet()
+        {
+            MonthStr = GetMonthName(Month);
+
+            DateTime dateTime = new DateTime(CurrentTime.Year, CurrentTime.Month, 1);
+            DayOfWeek dayOfWeek = dateTime.DayOfWeek;
+            FirstDayOfMonth = GetDayOfWeekAsNumber(dayOfWeek.ToString());
+        }
+
+        public void OnPostNextMonth(int month, int year)
+        {
+            Month = month + 1;
+            Year = year;
+            if (Month == 13)
+            {
+                Month = 1;
+                Year = year + 1;
+            }
+
+            DaysInMonth = DateTime.DaysInMonth(Year, Month);
+            DateTime dateTime = new DateTime(Year, Month, 1);
+            DayOfWeek dayOfWeek = dateTime.DayOfWeek;
+            FirstDayOfMonth = GetDayOfWeekAsNumber(dayOfWeek.ToString());
+            MonthStr = GetMonthName(Month);
+        }
+
+        public void OnPostPreviousMonth(int month, int year)
+        {
+            Month = month - 1;
+            Year = year;
+            if (Month == 0)
+            {
+                Month = 12;
+                Year = year - 1;
+            }
+
+            DaysInMonth = DateTime.DaysInMonth(Year, Month);
+
+            DateTime dateTime = new DateTime(Year, Month, 1);
+            DayOfWeek dayOfWeek = dateTime.DayOfWeek;
+            FirstDayOfMonth = GetDayOfWeekAsNumber(dayOfWeek.ToString());
+
+            MonthStr = GetMonthName(Month);
+        }
+
+        public string GetMonthName(int month)
+        {
+            string monthStr = "";
+            switch (Month)
+            {
+                case 1:
+                    monthStr = "Januar";
+                    break;
+                case 2:
+                    monthStr = "Februar";
+                    break;
+                case 3:
+                    monthStr = "Marts";
+                    break;
+                case 4:
+                    monthStr = "April";
+                    break;
+                case 5:
+                    monthStr = "Maj";
+                    break;
+                case 6:
+                    monthStr = "Juni";
+                    break;
+                case 7:
+                    monthStr = "Juli";
+                    break;
+                case 8:
+                    monthStr = "August";
+                    break;
+                case 9:
+                    monthStr = "September";
+                    break;
+                case 10:
+                    monthStr = "Oktober";
+                    break;
+                case 11:
+                    monthStr = "November";
+                    break;
+                case 12:
+                    monthStr = "December";
+                    break;
+            }
+            return monthStr;
+        }
+
+        public int GetDayOfWeekAsNumber(string dayOfWeek)
+        {
+            int day = 0;
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                    day = 1;
+                    break;
+                case "Tuesday":
+                    day = 2;
+                    break;
+                case "Wednesday":
+                    day = 3;
+                    break;
+                case "Thursday":
+                    day = 4;
+                    break;
+                case "Friday":
+                    day = 5;
+                    break;
+                case  "Saturday":
+                    day = 6;
+                    break;
+                case "Sunday":
+                    day = 7;
+                    break;
+            }
+            return day;
+        }
+    }
+}
